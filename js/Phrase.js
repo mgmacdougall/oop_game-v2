2888b82518c103855033a14e44178720974c2a75
Mike MacDougall <MacD>
2020-10-13 17:21:26 -0400

    app.js Addition of keyup for keystroke events, addition of phrase character creation in Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,26 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+const phraseContainer = document.querySelector('#phrase ul');
+
+class Phrase {
+	constructor(phrase) {
+		this.phrase = phrase.toLowerCase();
+	}
+
+	// This is the phrase to display
+	addPhraseToDisplay = () => {
+		const splitPhrase = this.phrase.split('');
+		splitPhrase.forEach((char) => {
+			const letterContainer = document.createElement('li');
+			letterContainer.className = '';
+			if (char === ' ') {
+				letterContainer.className = 'space';
+				letterContainer.innerText = ' ';
+			} else {
+				letterContainer.className += ' hide';
+				letterContainer.className += ' letter';
+				letterContainer.className += ` ${char}`;
+				letterContainer.innerText = char;
+			}
+			phraseContainer.appendChild(letterContainer);
+		});
+	};
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+const startBtn = document.getElementById('btn__reset');
+const overlay = document.getElementById('overlay');
+const keyboard = document.getElementById('qwerty');
+
+let game;
+let phrase;
+// Event listener for the start button
+startBtn.addEventListener('click', (e) => {
+	overlay.style.display = 'none';
+	game = new Game();
+	phrase = new Phrase('how are you');
+	phrase.addPhraseToDisplay();
+});
+
+// for mouse clicks
+keyboard.addEventListener('click', (e) => {});
+
+// for keyboard input events
+document.addEventListener('keyup', (e) => {
+	let charValue = getKeyStroke(e);
+	if (charValue) {
+		console.log(`The phrase is - ${phrase.phrase}`);
+	}
+});
+
+// function to get the keycode of the item pressed with keyboard
+// accepts the event + whether the event has fired
+const getKeyStroke = (e) => e.key;
