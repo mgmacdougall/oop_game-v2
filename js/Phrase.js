073c0509920a0ad72d938ed1c9a6acfd0551e9bd
Mike MacDougall <MacD>
2020-10-13 19:58:44 -0400

    Changes to support the button click, and the show tile functionality.  Also includes the click handler calling the handleInteraction

--- js/Phrase.js
+++ js/Phrase.js
@@ -10,12 +10,12 @@ class Phrase {
 		const splitPhrase = this.phrase.split('');
 		splitPhrase.forEach((char) => {
 			const letterContainer = document.createElement('li');
-			letterContainer.className = '';
+			// letterContainer.className = '';
 			if (char === ' ') {
 				letterContainer.className = 'space';
 				letterContainer.innerText = ' ';
 			} else {
-				letterContainer.className += ' hide';
+				letterContainer.className += 'hide';
 				letterContainer.className += ' letter';
 				letterContainer.className += ` ${char}`;
 				letterContainer.innerText = char;
@@ -23,4 +23,20 @@ class Phrase {
 			phraseContainer.appendChild(letterContainer);
 		});
 	};
+
+	phraseCheckLetter(letter) {
+		let letters = phraseContainer.getElementsByClassName(`${letter}`);
+		if (letters.length > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	showMatchedLetter(letter) {
+		let lettersInBoard = phraseContainer.getElementsByClassName(`${letter}`);
+		for (let i = 0; i < lettersInBoard.length; i++) {
+			lettersInBoard[i].classList.remove('hide');
+			lettersInBoard[i].classList.add('show');
+		}
+	}
 }
--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
+// Globals for UI Elements
 const startBtn = document.getElementById('btn__reset');
 const overlay = document.getElementById('overlay');
 const keyboard = document.getElementById('qwerty');
 
+// Dependant Globals for Game and Phrase
 let game;
 let phrase;
+
 // Event listener for the start button
 startBtn.addEventListener('click', (e) => {
 	overlay.style.display = 'none';
@@ -13,16 +16,28 @@ startBtn.addEventListener('click', (e) => {
 });
 
 // for mouse clicks
-keyboard.addEventListener('click', (e) => {});
+keyboard.addEventListener('click', (e) => {
+	handleInteraction(e.target.innerText);
+});
 
-// for keyboard input events
+// Listens for keyup events and runs the handle action
 document.addEventListener('keyup', (e) => {
 	let charValue = getKeyStroke(e);
 	if (charValue) {
-		console.log(`The phrase is - ${phrase.phrase}`);
+		handleInteraction(charValue);
 	}
 });
 
-// function to get the keycode of the item pressed with keyboard
-// accepts the event + whether the event has fired
+// function gets the key value pressed
 const getKeyStroke = (e) => e.key;
+
+// Main application functionality
+const handleInteraction = (key) => {
+	let hasLetter = phrase.phraseCheckLetter(key); // checks the value and will show if available
+	if (hasLetter) {
+		phrase.showMatchedLetter(key);
+		// also disable the button?
+	} else {
+		// this is where the loose life occurs
+	}
+};
