853cf27629660c9f633303b1e93cfbd5a9364fa1
Mike MacDougall <MacD>
2020-10-15 17:23:46 -0400

    Fix for click on keyboard causing actions to fire

--- js/app.js
+++ js/app.js
@@ -12,9 +12,19 @@ const phraseBoard = document.getElementsByClassName('letter');
 let game = new Game();
 let phrase;
 
+// Creates an event listenter for each of the values
+function bindKeyboardKeys() {
+	for (let i = 0; i < allKeys.length; i++) {
+		allKeys[i].addEventListener('click', (e) => {
+			game.handleInteraction(e);
+		});
+	}
+}
+
 // Event listener for the start button
 startBtn.addEventListener('click', (e) => {
 	game = new Game();
+	this.bindKeyboardKeys();
 	game.startGame();
 	phrase = game.getActivePhrase();
 
@@ -22,12 +32,8 @@ startBtn.addEventListener('click', (e) => {
 	phrase.addPhraseToDisplay();
 });
 
-// for mouse clicks
-keyboard.addEventListener('click', (e) => {
-	game.handleInteraction(e);
-});
-
 // Listens for keyup events and runs the handle action
 document.addEventListener('keyup', (e) => {
+	e.stopPropagation();
 	game.handleInteraction(e);
 });
